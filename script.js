ba1943197c0addef8055d71148cf0f014d4e7887
anhigo-s <anderson.higo2@gmail.com>
2022-01-17 23:04:22 -0300

    Init

---
+++ script.js
@@ -0,0 +1,29 @@
+/* Regras Codificador: 
+"e" é convertido para "enter" 
+"i" é convertido para "imes"
+"a" é convertido para "ai"
+"o" é convertido para "ober"
+"u" é convertido para "ufat"
+Apenas letras minúsculas
+Não permite acentuação   
+*/
+
+/* Regras Decodificador:
+"enter" é convertido para "e" 
+"imes" é convertido para "i"
+"ai" é convertido para "a"
+"ober" é convertido para "o"
+"ufat" é convertido para "u"
+Apenas letras minúsculas
+Não permite acentuação     
+*/
+
+function exibirMensagem()
+{
+	alert("Teste");
+}
+
+var btn1 = document.getElementById("btn1");
+var btn1 = document.getElementById("btn2");
+
+btn1.addEventListener("click", exibirMensagem);
