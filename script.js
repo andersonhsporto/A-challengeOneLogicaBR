c36aa6b936226a48997426148ded8d602033cd04
anhigo-s <anderson.higo2@gmail.com>
2022-01-18 00:45:10 -0300

    Script Convert

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+const criptograph = document.querySelector('#criptograph')
+const descriptograph = document.querySelector('#descriptograph')
+const copy = document.querySelector('#copy')
+const response = document.querySelector('#response');
+
+
+const tradutor = (regex, obj) => {
+    let text = document.querySelector('#message')
+    if(!/^[a-z0-9 ]*$/.test(text.value)) {
+        alert('Informe um texto sem acentuação, caracteres especiais ou letras maiúsculas!')
+        return 
+    }
+    document.querySelector('#response').innerHTML = obj.a ? 'criptografada:' : 'descriptografada:'
+    
+    response.value = text.value.replace(regex, key => obj[key])
+    text.value = ''
+}
+
+// $criptograph.prop('disabled', true);
+
+criptograph.addEventListener('click', () => {
+    tradutor(/[aeiou]/g, 
+        {'a':'ai','e':'enter','i':'imes','o':'ober','u':'ufat'})
+})
+
+
+descriptograph.addEventListener('click', () => {
+    tradutor(/ai|enter|imes|ober|ufat/g, 
+        {'ai':'a','enter':'e','imes':'i','ober':'o','ufat':'u'})
+})
+
+
+copy.addEventListener('click', () => {
+    response.select();
+    response.setSelectionRange(0, 99999); /*For mobile devices */
+    navigator.clipboard.writeText(response.value);
+})
 /* Regras Codificador: 
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
@@ -17,13 +54,3 @@ Não permite acentuação
 Apenas letras minúsculas
 Não permite acentuação     
 */
-
-function exibirMensagem()
-{
-	alert("Teste");
-}
-
-var btn1 = document.getElementById("btn1");
-var btn1 = document.getElementById("btn2");
-
-btn1.addEventListener("click", exibirMensagem);
